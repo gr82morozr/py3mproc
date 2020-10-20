f9eaf7de24611622e83b2b70244cf1c1ef6e4150
Great Tomorrow <gr82morozr@gmail.com>
2020-10-20 11:15:21 +1100

    bump up version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name          = 'py3mproc',
   packages      = ['py3mproc'],
-  version       = '0.1.5',
+  version       = '0.1.6',
   description   = 'A Python3 multiprocessing workflow',
-  author        = 'Fan Yang',
+  author        = 'Great Tomorrow',
   author_email  = 'gr82morozr@gmail.com',
   url           = 'https://github.com/gr82morozr/py3mproc.git',  
   download_url  = 'https://github.com/gr82morozr/py3mproc.git', 
   keywords      = ['Python3', 'Multiprocessing', 'Workflow', 'Framework'], 
   classifiers   = [],
-  install_requires=[ 'psutil']
+  install_requires=[ 'psutil', 'py3toolbox']
 )
